bc52db905adcf51477389418bb502d4499183bdf
Michael Hoang <enzime@users.noreply.github.com>
2016-06-13 20:05:37 +1000

    Add initial canvas sync (#2)

--- server.js
+++ server.js
@@ -5,23 +5,50 @@ var io = require('socket.io')(http);
 
 app.use(express.static('public'));
 app.get('/', function(req, res){
-	  res.sendFile(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
+
+var strokes = [];
 
 io.on('connection', function(socket) {
-  socket.on('drawClick', function(data) {
-    socket.broadcast.emit('draw', {
-      x: data.x,
-      y: data.y,
-      type: data.type
+    socket.on('getCurrent', function() {
+        socket.emit('current', strokes.length - 1);
     });
-  });
 
-  socket.on('clear', function(){
-  	socket.broadcast.emit('clear');
-  })
+    socket.on('getStroke', function(id) {
+        if (strokes[id]) {
+            socket.emit('draw', strokes[id]);
+        }
+    });
+
+    socket.on('getStrokes', function(data) {
+        if (strokes[data.start] && strokes[data.end - 1]) {
+            socket.emit('drawStrokes', strokes.slice(data.start, data.end));
+        }
+    });
+
+    socket.on('drawClick', function(data) {
+        let id = strokes.push(data) - 1;
+        strokes[id]['id'] = id;
+
+        console.log(id, strokes[id]);
+
+        socket.emit('drawReceived', {
+            id: id,
+            data: data
+        });
+
+        socket.broadcast.emit('draw', strokes[id]);
+    });
+
+    socket.on('clear', function() {
+        strokes = [];
+
+        socket.broadcast.emit('clear');
+        console.log('clearing');
+    });
 });
 
-http.listen(4000, function(){
-	  console.log('listening on *:4000');
+http.listen(4000, function() {
+    console.log('listening on *:4000');
 });
